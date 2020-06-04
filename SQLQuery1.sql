f69267cf3aa1a86c3232c64063def89dfb115cc8
alx.fad3 <alx.fad3@gmail.com>
2020-06-04 17:50:51 +0300

    удалены лишние файлы

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT 
-COUNT(fm.Name) CountFiles, 
-ROUND(SUM((fm.Size / 1024) / 1024), 2)  SizeMb, 
-dm.Path 
-FROM FileModels fm
-JOIN DirectoryModels dm ON dm.Id = fm.DirectoryId
-GROUP BY dm.Path
